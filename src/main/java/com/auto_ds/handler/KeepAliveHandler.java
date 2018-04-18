bfa2831d9dea6e454a4365412729a9d807add7df
cod634 <cod634.steam@gmail.com>
2018-04-18 19:48:32 +0200

    aa

--- src/main/java/com/auto_ds/handler/KeepAliveHandler.java
+++ src/main/java/com/auto_ds/handler/KeepAliveHandler.java
@@ -1,10 +1,14 @@
 package com.auto_ds.handler;
 
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
@@ -14,7 +18,7 @@ public class KeepAliveHandler {
     public static final String RUNNING = "running";
     public static final String STOPPED = "stopped";
 
-    private final String herokuUrl = "";
+    private final String HEROKU_URL = "https://warm-oasis-36897.herokuapp.com/";
     private String operationalStatus = STOPPED;
 
     //private int INTERVALS = 300000;
@@ -29,15 +33,18 @@ public class KeepAliveHandler {
     public void keepAlive() {
         logger.info("STARTING TO KEEP ALIVE...");
         operationalStatus = RUNNING;
-
         Runnable task = () -> {
-            while (operationalStatus.equals(RUNNING)){
+            while (operationalStatus.equals(RUNNING)) {
                 logger.info("TEST");
                 try {
-                    logger.info("WAITING FOR: " + INTERVALS/1000/60 + "(min)");
+                    executeGet();
+                    logger.info("WAITING FOR: " + INTERVALS / 1000 / 60 + "(min)");
                     Thread.sleep(INTERVALS);
                 } catch (InterruptedException e) {
                     logger.error("\n\n --- INTERVAL THREAD INTERRUPTED --- \n\n");
+                } catch (IOException e) {
+                    logger.error("\n\n --- GET ERROR --- \n\n");
+
                 }
             }
         };
@@ -48,4 +55,12 @@ public class KeepAliveHandler {
         logger.info("SHUTTING DOWN");
         operationalStatus = STOPPED;
     }
+
+    private void executeGet() throws IOException {
+        HttpClient httpClient = HttpClientBuilder.create().disableRedirectHandling().build();
+        HttpGet get = new HttpGet(HEROKU_URL);
+        httpClient.execute(get);
+
+    }
+
 }
